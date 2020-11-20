fcaa0d85955cc0517dcc40fc0308bacfe739b964
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2020-11-21 02:08:34 +0700

    Praktikum Rekayasa Web pertemuan 1
    Tugas Praktikum Rekayasa Web pertemuan 1

---
+++ latihan1.php
@@ -0,0 +1,17 @@
+<?php
+
+$mahasiswa = [
+    [
+        "nama" => "Sandhika Galih",
+        "nrp" => "043040023",
+        "email" => "sandhikagalih@unpas.ac.id"
+    ],
+    [
+        "nama" => "Sandhika Galih",
+        "nrp" => "043040023",
+        "email" => "sandhikagalih@unpas.ac.id"
+    ]
+];
+
+$data = json_encode($mahasiswa);
+echo $data;
---
+++ script.js
@@ -0,0 +1,7 @@
+let mahasiswa = {
+    nama: "sandhika galih",
+    nrp: "030403023",
+    email: "sandhikagalih@unpas.ac.id"
+}
+
+console.log(JSON.stringify(mahasiswa));
